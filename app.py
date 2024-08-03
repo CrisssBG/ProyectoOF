024af09c88f5da2f4786d2cbdcdc76ea12f3e703
CristopherBG <cristopherbor1505@gmail.com>
2024-08-02 21:33:30 -0500

    html a raiz

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 from flask import Flask
 
 # Application initializations
-app = Flask (__name__)
+# app = Flask (__name__)
+app = Flask(__name__, template_folder='.')
 
 #Settings
 app.secret_key = 'mysecretkey'
